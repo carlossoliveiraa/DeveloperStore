0b86cbb67bfc7e6fd12bd7f9c73a9c89cccc512e
Carlos Alberto de Oliveira <carlossoliveiraa@hotmail.com>
2025-03-30 16:35:08 -0300

    finalize Infrastructure layer with EF Core, repository pattern, and integration tests using InMemory
    finalize Infrastructure layer with EF Core, repository pattern, and integration tests using InMemory

---
+++ DeveloperStore.Sales.Infrastructure/Persistence/Mappings/SaleItemMap.cs
@@ -0,0 +1,44 @@
+﻿using DeveloperStore.Sales.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DeveloperStore.Sales.Infrastructure.Persistence.Mappings
+{
+    public sealed class SaleItemMap : IEntityTypeConfiguration<SaleItem>
+    {
+        public void Configure(EntityTypeBuilder<SaleItem> builder)
+        {
+            builder.ToTable("SaleItems");
+
+            builder.HasKey(item => item.Id);
+
+            builder.Property(item => item.ProductId)
+                .IsRequired();
+
+            builder.Property(item => item.ProductName)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(item => item.Quantity)
+                .IsRequired();
+
+            builder.Property(item => item.UnitPrice)
+                .IsRequired()
+                .HasPrecision(18, 2);
+
+            builder.Property(item => item.Discount)
+                .IsRequired()
+                .HasPrecision(18, 2);
+
+            builder.Property(item => item.Total)
+                .HasPrecision(18, 2)
+                .HasComputedColumnSql("([UnitPrice] * [Quantity]) - [Discount]", stored: true); // Opcional, se usar SQL Server
+
+            //Do not try to map this property, it only exists in the domain
+            builder.Ignore(x => x.Total);
+                   
+            builder.Property<Guid>("SaleId")
+                .IsRequired();
+        }
+    }
+}
---
+++ DeveloperStore.Sales.Infrastructure/TestUtils/TestSalesDbContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using DeveloperStore.Sales.Infrastructure.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace DeveloperStore.Sales.Infrastructure.TestUtils
+{
+    public static class TestSalesDbContextFactory
+    {
+        public static SalesDbContext CreateInMemoryContext(string dbName)
+        {
+            var options = new DbContextOptionsBuilder<SalesDbContext>()
+                .UseInMemoryDatabase(databaseName: dbName)
+                .Options;
+
+            return new SalesDbContext(options);
+        }
+    }
+}
